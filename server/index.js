953dd63309fec5dcc0258b0dcbb710607c18b564
zoe <z.zoelec2@gmail.com>
2017-12-23 16:54:15 -0800

    Deleting some things

--- server/index.js
+++
@@ -1,7 +0,0 @@
-import TodoistAPI from 'todoist-js';
-
-const todoist = new TodoistAPI('663c247fdad54d4ea4ddd4c290ef3607');
-
-todoist.completed.get_stats().then(stats => {
-  console.log(stats.karma_trend);
-});
